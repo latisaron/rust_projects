83297f2f5c8ea8704676799f010ad77d9b47fe3a
Aron <latis.aron@gmail.com>
2023-12-31 02:21:48 +0200

    cli_utils basics, no errors

---
+++ cli_utils/target/debug/cli_utils.d
@@ -0,0 +1,1 @@
+C:\Users\omega\Desktop\rust\easy-project\cli_utils\target\debug\cli_utils.exe: C:\Users\omega\Desktop\rust\easy-project\cli_utils\src\main.rs
---
+++ cli_utils/target/debug/deps/cli_utils.d
@@ -0,0 +1,5 @@
+C:\Users\omega\Desktop\rust\easy-project\cli_utils\target\debug\deps\cli_utils.exe: src\main.rs
+
+C:\Users\omega\Desktop\rust\easy-project\cli_utils\target\debug\deps\cli_utils.d: src\main.rs
+
+src\main.rs:
